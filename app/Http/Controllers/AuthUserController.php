b1106700b6dd6d5d0ece07e498e2c044fa18368f
AlekseiZam <aleksei.zam2000@yandex.ru>
2023-03-05 13:02:24 +0400

    Second commit

---
+++ app/Http/Controllers/AuthUserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class AuthUserController extends Controller
+{
+    public function auth()
+    {
+//
+    }
+}
